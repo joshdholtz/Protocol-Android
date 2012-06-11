08b308c95166e6f272ffb916e2268ae8ace51494
Josh <joshdholtz.junk@gmail.com>
2012-06-11 10:07:37 -0500

    Committing quick fix for params missing question mark

--- src/com/joshdholtz/protocol/lib/Protocol.java
+++ src/com/joshdholtz/protocol/lib/Protocol.java
@@ -567,8 +567,8 @@ public class Protocol {
 	
 	private String paramsToString(Map<String, Object> params) {
 		String paramsStr = "";
-		if (params != null) {
-			paramsStr += "";
+		if (params != null && params.size() > 0) {
+			paramsStr += "?";
 			try {
 				List<String> keys = new ArrayList<String>(params.keySet());
 				for (int i = 0; i < keys.size(); ++i) {
