6e97bb15f59d6b41e7e2749edf81a83a3831a433
Josh Holtz <josh@rokkincat.com>
2012-12-27 22:26:20 -0600

    Committing response, status, and data getters to ProtocolResponseHandler

--- src/com/joshdholtz/protocol/lib/responses/ProtocolResponseHandler.java
+++ src/com/joshdholtz/protocol/lib/responses/ProtocolResponseHandler.java
@@ -4,11 +4,47 @@ import org.apache.http.HttpResponse;
 
 public abstract class ProtocolResponseHandler {
 	
+	private HttpResponse response;
+	private int status;
+	private byte[] data;
+	
+	/**
+	 * Sets the response, status, and data and executes the handleResponse method for
+	 * this class and any child classes. 
+	 * 
+	 * @param response
+	 * @param status
+	 * @param data
+	 */
 	public void generateResponse(HttpResponse response, int status, byte[] data) {
+		this.response = response;
+		this.status = status;
+		this.data = data;
 		
 		handleResponse(response, status, data);
 	}
 	
+	/**
+	 * @return the response
+	 */
+	public HttpResponse getResponse() {
+		return response;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public int getStatus() {
+		return status;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public byte[] getData() {
+		return data;
+	}
+
 	public abstract void handleResponse(HttpResponse response, int status, byte[] data);
 	
 }
